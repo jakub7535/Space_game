369ceaf5185ea71ad4616a2e775e2f6ae5424ad3
jakub7535 <jakub7535@gmail.com>
2020-11-13 22:50:55 +0100

    adding laser to spaceship, adding saving scores, small refactoring

--- game.py
+++ game.py
@@ -1,21 +1,22 @@
 import random
 import pygame
-from time import sleep
-from copy import deepcopy
-from player import Player, Resources_Obstacles, Laser
+from color import Color
+from space_objects import Resources_Obstacles, Laser
 
 class Game:
-    n_levels = 6
+    n_levels = 10
     initial_speed = 5
     speed_jump = 1
+    max_speed = 9
     pnt_nex_lvl = 50
     
     resources_obstacles =[ 
-            {"img":"comet.png", "sound":"explosion.wav", "size":100, "life":-10, "score":0},
-           {"img":"ufo.png", "sound":"explosion.wav", "size":80, "life":-15, "score":0},
-           {"img":"diamond.png", "sound":"explosion.wav", "size":60, "life":0, "score":30}
+            {"img":"comet.png", "sound":"explosion.wav", "size":60, "life":-20, "score":0},
+            {"img":"ufo.png", "sound":"explosion.wav", "size":80, "life":-15, "score":0},
+            {"img":"diamond.png", "sound":"explosion.wav", "size":60, "life":0, "score":20},
+            {"img": "tools.png", "sound": "explosion.wav", "size": 40, "life": 10, "score": 0}
         ]
-    probability_of_objects = [0.2, 0.3, 0.5]
+    probability_of_objects = [0.3, 0.4, 0.25, 0.05]
     
     def __init__(self):
         self.life = 100
@@ -24,15 +25,24 @@ class Game:
         self.level = 1
         self.resources_obstacles_list = []
         self.laser_list = []
-    
-    
+        self.name_player = ""
+        self.record_names, self.record_scores = self.read_records()
+        self.ammunition = 100
+
+    def read_records(self):
+        with open("assets/records.txt", 'r') as f:
+            records = f.readlines()
+        record_names = [line.split()[0] for line in records]
+        record_scores = [int(line.split()[1]) for line in records]
+        return record_names, record_scores
+
     """
     Function creates objects 'above' the game screen. With every iteration game lowers them down
     number of pixels equal to Game.speed. When sumaric number of passed pixels surpass or equals to 
     height of the screen function is reapeted.
     """
     def create_resources_obstacles(self, screen_width, screen_height):
-        resources_obstacles_number = random.randint(3,10)
+        resources_obstacles_number = random.randint(3+self.level, 10+self.level)
         for i in range(resources_obstacles_number):
             # parameters of new objects of Obstacles_Resources
             ro_param = random.choices(self.resources_obstacles, self.probability_of_objects)[0]            
@@ -45,8 +55,10 @@ class Game:
             self.resources_obstacles_list.append(new_ro)
 
     def create_laser(self, player_x, player_y, player_size):
-        laser = Laser(player_x, player_y, player_size)
-        self.laser_list.append(laser)
+        if self.ammunition >= 20:
+            laser = Laser(player_x, player_y, player_size)
+            self.laser_list.append(laser)
+            self.ammunition -= 20
 
     def update_resources_obstacles_positions(self, screen_height):
         for ro in self.resources_obstacles_list:
@@ -55,7 +67,7 @@ class Game:
             else:
                 self.resources_obstacles_list.remove(ro)
 
-    def update_lasers_positions(self, screen_height):
+    def update_lasers_positions(self, pixels):
         for laser in self.laser_list:
             if laser.y > 0:
                 laser.y -= self.speed
@@ -65,12 +77,12 @@ class Game:
     def set_level_speed(self):
         self.level = min(int(self.score/self.pnt_nex_lvl),self.n_levels)
         self.speed = min(self.initial_speed + self.level*self.speed_jump, 
-                         self.initial_speed + self.n_levels*self.speed_jump) 
+                         self.max_speed)
         
     def collision_check(self, player):
         for ro in self.resources_obstacles_list:
             if ro.detect_collision(player):
-                self.life += ro.life
+                self.life = min(self.life+ro.life, 100)
                 self.score += ro.score
                 self.resources_obstacles_list.remove(ro)
                 #enemy.sound.play()
@@ -86,13 +98,38 @@ class Game:
         return False
     
     # function run after player have lost
-    def end_game(screen, game, player):
-        screen.update_screen(game, player)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.display.quit()
-                pygame.quit()
-                return True
-            else:
-                return False
+    def end_game(self, screen, game, index):
+        screen.refresh_background()
+        screen.draw_centered_text(text="YOU LOST!", x=0.5*screen.width, y=0.2*screen.height,
+                         color=Color.RED, font_type="cambria", font_size=80)
+        screen.draw_centered_text(text=f"YOUR SCORE: {game.score}", x=0.5*screen.width, y=0.3*screen.height,
+                         color=Color.RED, font_type="cambria", font_size=50)
 
+        if index is not None:
+            self.record_names[index] = self.name_player
+            self.record_scores[index] = game.score
+
+            screen.draw_centered_text(text=f"ENTER YOUR NAME", x=0.5 * screen.width,
+                                      y=0.4 * screen.height,color=Color.RED, font_type="cambria", font_size=40)
+
+            for i in range(len(self.record_scores)):
+                screen.draw_centered_text(text=f"{i+1}. {self.record_names[i]}: {self.record_scores[i]}", x=0.5 * screen.width,
+                                          y=(0.5+0.05*i) * screen.height,color=Color.RED, font_type="cambria", font_size=40)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return True
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        with open("assets/" + 'records.txt', 'w') as file:
+                            for i in range(len(self.record_scores)):
+                                file.write(f"{self.record_names[i]} {self.record_scores[i]}\n")
+                        return True
+                    elif event.key == pygame.K_BACKSPACE:
+                        self.name_player = self.name_player[:-1]
+                    else:
+                        self.name_player += event.unicode.upper()
+
+        else:
+            for event in pygame.event.get():
+                return event.type == pygame.QUIT
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 from time import sleep
-from player import Player, Space_Objects
+from space_objects import Player, Laser
 from screen import Screen
 from game import Game
 
@@ -14,6 +14,7 @@ def play_game(screen, player, game):
     #     pygame.mixer.music.play(-1)
     # how many pixel passed
     pixels = 0
+    index_calculated = False
     while True:
         if game.life > 0:
             game.collision_check(player)
@@ -36,25 +37,32 @@ def play_game(screen, player, game):
             # when to create new resources and obstacles
             if pixels % screen.height >= 0 and pixels % screen.height < game.speed:
                 game.create_resources_obstacles(screen.width, screen.height)
+                game.ammunition = min(game.ammunition + 20, 100)
             pixels += game.speed
 
             game.update_resources_obstacles_positions(screen.height)
-            game.update_lasers_positions(screen.height)
+            game.update_lasers_positions(pixels)
             game.set_level_speed()
             screen.update_screen(game, player)
-
+            pygame.display.update()
         else:
-            game_over = Game.end_game(screen, game, player)
+            pygame.display.update()
+            # Where player would be in top scores
+            if not index_calculated:
+                index = next((i for i, record_score in enumerate(game.record_scores)
+                              if game.score > record_score), None)
+                index_calculated=True
+            game_over = game.end_game(screen, game, index)
             if game_over:
+                pygame.display.quit()
+                pygame.quit()
                 sys.exit()
                 break
 
-
 if __name__ == "__main__":
-    path_assets = "assets/car/"
     pygame.init()
-    screen = Screen(path_assets=path_assets)
-    player = Player(x=screen.width / 2, y=screen.height - 100, path_assets=path_assets)
+    screen = Screen()
+    player = Player(x=screen.width / 2, y=screen.height - 100)
     game = Game()
 
     play_game(screen, player, game)
